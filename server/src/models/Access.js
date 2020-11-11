f3880fc14a344da9ea28f9ee70b2918493733c53
breezycamel <phuocho99@gmail.com>
2020-11-11 15:15:53 -0500

    added access model

---
+++ server/src/models/Access.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AccessSchema = new Schema({
+	file_id: { type: String, required: true, index: { unique: true } },
+	file_name: {type: String, required: true},
+	owner: {type: String, required: true},
+	user: {type: [String], required: true}
+});
+
+module.exports = mongoose.model('Access', AccessSchema);
